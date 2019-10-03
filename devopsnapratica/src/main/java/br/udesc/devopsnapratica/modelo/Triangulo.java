7a1279a7ba083dff602cbfd4670ae5af3984e09c
Mattheus da Hora França (GOVBR - DDP) <Mattheus.Franca@cetil.com.br>
2019-10-03 02:05:45 -0300

    Funcionalidade para verificar tipo de triangulo]

---
+++ devopsnapratica/src/main/java/br/udesc/devopsnapratica/modelo/Triangulo.java
@@ -0,0 +1,66 @@
+package br.udesc.devopsnapratica.modelo;
+
+import br.udesc.devopsnapratica.exception.NaoFormaTrianguloException;
+
+public class Triangulo {
+
+	private int ladoA;
+	private int ladoB;
+	private int ladoC;
+	
+	public Triangulo() {
+		
+	}
+	
+	public Triangulo(int ladoA, int ladoB, int ladoC) throws NaoFormaTrianguloException {
+		
+		if(!formaTriangulo(ladoA, ladoB, ladoC))
+			throw new  NaoFormaTrianguloException();
+		this.ladoA = ladoA;
+		this.ladoB = ladoB;
+		this.ladoC = ladoC;
+	}
+
+	private boolean formaTriangulo(double ladoA, double ladoB, double ladoC) {
+		if(ladoA < (ladoB+ladoC) && ladoB < (ladoA + ladoC) && ladoC < (ladoA + ladoB)) {
+			return true;			
+		}
+		return false;
+	}
+	
+	public int getLadoA() {
+		return ladoA;
+	}
+
+	public int getLadoB() {
+		return ladoB;
+	}
+
+	public int getLadoC() {
+		return ladoC;
+	}
+
+	public TipoTriangulo getTipoTriangulo() {
+		if (ladoA == ladoB || ladoB == ladoC || ladoC == ladoA) {
+            if (ladoA == ladoB && ladoB == ladoC) {
+                return TipoTriangulo.EQUILATERO;
+            } else {
+            	return TipoTriangulo.ISOSCELES;
+            }
+        }
+        else return TipoTriangulo.ESCALENO;
+     }
+
+	public void setLadoA(int ladoA) {
+		this.ladoA = ladoA;
+	}
+
+	public void setLadoB(int ladoB) {
+		this.ladoB = ladoB;
+	}
+
+	public void setLadoC(int ladoC) {
+		this.ladoC = ladoC;
+	}
+
+}
